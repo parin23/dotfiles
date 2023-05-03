ebe4d7fbc2b90b02b17cf29bcb5565733f6dd25f
Parin Jasoliya <parinjasoliya@gmail.com>
2023-05-04 02:19:46 +0530

    Fix nvim-cmp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,3 +10,23 @@ require('lspconfig').clangd.setup{}
 
 lsp.setup()
 
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  },
+--  sources = {
+--    { name = 'path' }
+--  }
+})
